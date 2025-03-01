87324918bca1dffc7495c54825d09678ad3bd623
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 13:40:14 +0530

    pattern 13

--- patterns.cpp
+++ patterns.cpp
@@ -131,9 +131,18 @@ void pattern12(int n){
         cout << endl;
     }
 }
+void pattern13(int n){
+    int val = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++, val++){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     int n;
     cin >> n;
-    pattern12(n);
+    pattern13(n);
 } 
