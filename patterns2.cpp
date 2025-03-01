3ffadbe53297f3121b87c52fb66f0d002e3ccd25
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 15:42:05 +0530

    pattern 3

--- patterns2.cpp
+++ patterns2.cpp
@@ -20,10 +20,20 @@ void pattern2(int n){
         cout << endl;
     }
 }
-
+void pattern3(int n){
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n - i; k++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 int main(){
     int n;
     cin >> n;
 
-    pattern2(n);
+    pattern3(n);
 }
