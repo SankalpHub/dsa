349b8f2795b84e130901e2b71c5ca30ffbd16d90
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 15:38:12 +0530

    pattern 2

--- patterns2.cpp
+++ patterns2.cpp
@@ -9,10 +9,21 @@ void pattern1(int n){
         cout << endl;
     }
 }
+void pattern2(int n){
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n - i; k++){
+            cout << "  ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int n;
     cin >> n;
 
-    pattern1(n);
+    pattern2(n);
 }
