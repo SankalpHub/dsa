260e3d743b008e4411717b6ff95eaf1117a91cb7
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 14:39:28 +0530

    pattern 20

--- patterns.cpp
+++ patterns.cpp
@@ -228,9 +228,28 @@ void pattern19(int n){
         cout << endl;
     }
 }
+void pattern20(int n){
+    for(int i = 1; i < 2 * n; i++){
+        int val = i;
+        if(i > n) val = 2 * n - i;
 
+        //stars
+        for(int j = 1; j <= val; j++){
+            cout << "*";
+        }
+        //spaces
+        for(int k = 1; k <= 2 * (n - val); k++){
+            cout << " ";
+        }
+        //stars
+        for(int j = 1; j <= val; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main() {
     int n;
     cin >> n;
-    pattern19(n);
+    pattern20(n);
 } 
