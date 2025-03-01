181e4ea48ca2dbde513d33f1d460c9886edaf248
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 12:28:32 +0530

    pattern 9

--- patterns.cpp
+++ patterns.cpp
@@ -75,9 +75,35 @@ void pattern8(int n){
         cout << endl;
     }
 }
+void pattern9(int n){
+    //upper half
+    for(int i = 1; i <= n; i++){
+        //spaces                            
+        for(int k = 1; k <= n - i; k++){
+            cout << " ";
+        }                                           
+        //stars                                     
+        for(int j = 1; j <= 2 * i - 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    //lower half
+    for(int i = n; i >= 1; i--){
+        //spaces
+        for(int k = 1; k <= n - i; k++){
+            cout << " ";
+        }
+        //stars
+        for(int j = 1; j <= 2 * i - 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     int n;
     cin >> n;
-    pattern8(n);
+    pattern9(n);
 } 
