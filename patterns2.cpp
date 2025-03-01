873442f0191271bddefa33176e5adbd09d270bac
SankalpHub <sankalpt0202@gmail.com>
2025-03-01 16:40:48 +0530

    pattern 11

--- patterns2.cpp
+++ patterns2.cpp
@@ -112,10 +112,24 @@ void pattern10(int n){
         cout << endl;
     }
 }
+void pattern11(int n){
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n - i; k++){
+            cout << "  ";
+        }
+        for(int j = 1; j <= 2 * i - 1; j++){
+            if(i > 1 && i < n && j > 1 && j < 2 * i - 1)
+            cout << "  ";
+
+            else cout << "* ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int n;
     cin >> n;
 
-    pattern10(n);
+    pattern11(n);
 }
